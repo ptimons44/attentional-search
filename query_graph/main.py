9e75e0433993b90b8d40a2a49ba4505f1e406c76
ptimons44 <patricktimons@gmail.com>
2023-06-20 22:04:07 +0930

    Update main.py
    NTS: fetch a window around the sentences that we want to keep

--- query_graph/main.py
+++ query_graph/main.py
@@ -4,22 +4,19 @@ from extraction import get_k_most_similar_sents
 
 import json
 
-def create_query_graph(query, num_search_results):
+def create_query_graph(query, num_search_results, num_nodes=100):
     """returns the query graph associated for a particular input query
 
     Args:
         query (string): user's (textual) query, such as a question or research topic
     """
-    # search_queries = generate_search_queries(query)
-    search_queries = ['The 2020 election', 'Trump', 'fraudulent voting machines'] # 'electronic ballots', 'voter fraud', 'mail-in voting', 'swing states', 'election interference', 'campaign finance', 'polling data', 'voting irregularities', 'ballot counting', 'legal challenges', 'election results'
+    search_queries = generate_search_queries(query)
     content = dict()
     for search_query in search_queries:
         scraped_content = get_top_k_content(search_query, k=num_search_results)
         content |= scraped_content
-    with open('/Users/patricktimons/Documents/GitHub/query-graph/query_graph/text.json', "w") as f:
-        json.dump(content, f)
-    # result = get_k_most_similar_sents(content, query)
-    # return result
+    nodes = get_k_most_similar_sents(content, query, k=num_nodes)
+    return nodes
 
 if __name__ == "__main__":
     query = "The 2020 election was stolen from Trump because of fraudulent voting machines and electronic ballots."
