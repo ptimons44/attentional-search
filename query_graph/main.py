552d10b95c53b955b3c4e9a6a2b4fc258071515c
ptimons44 <patricktimons@gmail.com>
2023-06-26 17:38:48 +0930

    almost everything lives in the Researcher class

--- query_graph/main.py
+++ query_graph/main.py
@@ -38,74 +38,23 @@ def create_query_graph(query, num_search_results, nlp, num_nodes=100, context_wi
     """
     gpt_response = ask_gpt_query(query)
     search_queries, gpt_keyword_sentence_mapping = generate_search_queries(query, gpt_response, nlp)
-    researcher = Researcher.Researcher(search_queries, num_search_results)
-
-
-    def create_page(search_query, url, pages):
-        page = Researcher.Page(search_query,url)
-        pages.append(page)
-
-    pages = []
-    threads = []
-    for (search_query, url) in researcher.urls:
-        threads.append(threading.Thread(target=create_page, args=(search_query,url,pages)))
-    
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-    
-    print(len(pages))
-    for page in pages:
-        if page.content:
-            print(page.sentences)
-            break
-
-    # print("maping:\n", gpt_keyword_sentence_mapping)
-    # print("search_queries", search_queries)
-    # content = dict()
-    # for search_query in search_queries:
-    #     scraped_content = get_top_k_content(search_query, content, k=num_search_results)
-    #     content |= scraped_content
-    # with open("temp.json", "w") as f:
-    #     json_object = json.dumps(content, indent=4)
-    #     f.write(json_object)
-    # #     return
-    # with open("temp.json", "r") as f:
-    #     content = json.load(f)
-    # nodes = get_k_most_similar_sents(content, query, k=num_nodes, context_window=context_window)
-
-    # classifier = pipeline('zero-shot-classification', model='roberta-large-mnli')
-
-    # nlp = spacy.load("en_core_web_sm")
-    # gpt_sentences = list(nlp(gpt_response).sents)
-
-    # graph = dict()
-    # for ix, (similarity,source,sent,context,search_phrase) in enumerate(nodes):
-    #     # try:
-    #     print("keyword extracted: ", gpt_keyword_from_query(search_phrase))
-    #     sentence_index = gpt_keyword_sentence_mapping[gpt_keyword_from_query(search_phrase)]
-    #     print("sentence index", sentence_index)
-    #     print("sentence: ", gpt_sentences[sentence_index])
-    #     relation = classify_relationship(sent, gpt_sentences[sentence_index], classifier)
-    #     print("relation", relation)
-    #     graph[context] = (relation, gpt_sentences[sentence_index])
-
-    # except:
-    #         print("RegexError: GPT keyword not found")
-
-
-    # return graph
+    researcher = Researcher.Researcher(query, search_queries, gpt_response, num_search_results)
+    nodes = researcher.top_k_similar_sentences()
+    print("num nodes", len(nodes))
+    print(nodes[0])
+    for sentence in nodes:
+        sentence.get_relation_to_query(researcher.query)
+    return nodes
 
 if __name__ == "__main__":
     nlp = spacy.load(config.language_model())
-    # query = "Was the 2020 election was stolen from Trump because of fraudulent voting machines and electronic ballots?"
-    query = "Was Issac Newton the first person to discover gravity??"
+    query = "Was the 2020 election was stolen from Trump because of fraudulent voting machines and electronic ballots?"
+    # query = "Issac Newton was the first person to discover gravity."
+    # query = "Neil Armstrong never went to space; he was a paid actor by the inner circle AKA NASA."
     graph = create_query_graph(query, 3, nlp)
-    for node in graph:
-        print("web sentence:", node)
-        print(graph[node])
-        print("\n")
-    # for ix, (similarity,source,sent,context,search_phrase) in enumerate(result):
-    #     print(ix, ".", search_phrase, ":", context)
-    #     print("\n\n")
+    for node in (graph):
+        print(node.relation_to_query)
+        print(node.context)
+        print("\n\n")
+    # search = Researcher.Search(query, 5)
+    # print(search.search_google())
