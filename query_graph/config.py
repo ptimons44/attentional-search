fb1038d1b4857fe0caef3b1adda8f0433cbe2488
ptimons44 <103891668+ptimons44@users.noreply.github.com>
2023-07-03 14:48:35 +0930

    Create config.py

---
+++ query_graph/config.py
@@ -0,0 +1,23 @@
+def OPENAI_APIKEY():
+    return ""
+
+def OPENAI_MODEL():
+    return "gpt-3.5-turbo"
+
+def GGLSEARCH_APIKEY():
+    return ""
+
+def GGLSEARCH_URL():
+    return "https://www.googleapis.com/customsearch/v1"
+
+def GGL_SE():
+    return ""
+
+def language_model():
+    return "en_core_web_sm"
+
+def GPT_model():
+    return "gpt-3.5-turbo"
+
+def GPT_context_window():
+    return 4096
