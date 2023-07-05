07a85287888c4bb8f6c1f370024d0671a1d6101c
ptimons44 <patricktimons@gmail.com>
2023-07-05 13:22:10 +0930

    seperated batched sentence embeddings & content retrevial

--- query_graph/config.py
+++ query_graph/config.py
@@ -1,17 +1,17 @@
 def OPENAI_APIKEY():
-    return ""
+    return "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"
 
 def OPENAI_MODEL():
     return "gpt-3.5-turbo"
 
 def GGLSEARCH_APIKEY():
-    return ""
+    return "AIzaSyDlk0B8q6iE36TQmvDLtD29KjOFPjyi4hQ" # "AIzaSyAWhYs7CvMkCVvKCpcrppmer0htz7CpurE" #
 
 def GGLSEARCH_URL():
     return "https://www.googleapis.com/customsearch/v1"
 
 def GGL_SE():
-    return ""
+    return "53e1e6633fc3e4dae"
 
 def language_model():
     return "en_core_web_sm"
